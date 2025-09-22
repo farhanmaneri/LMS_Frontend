952e59a4604b28af5abd493b8aa3538b70c5bb5d
farhanmaneri <farhanmaneri@gmail.com>
2025-09-22 18:59:36 +0500

    vercel.json file add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./index.css";
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Signup from "./pages/CreateUser";
 import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import PrivateRoute from "./components/PrivateRoute";
@@ -10,16 +9,21 @@ import Layout from "./components/Layout";
 import ChangePassword from "./pages/ChangePassword";
 import NotFound from "./components/NotFound";
 import ResetPassword from "./pages/ResetPassword";
+
 function App() {
+  // Add this for debugging
+  console.log("App component rendered");
+  console.log("Current pathname:", window.location.pathname);
+  console.log("Current search params:", window.location.search);
 
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-      {/* Public routes */}
+      {/* Public routes - These should be BEFORE the catch-all route */}
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/signup" element={<Signup />} /> */}
       <Route path="/reset-password" element={<ResetPassword />} />
+      {/* <Route path="/signup" element={<Signup />} /> */}
 
       {/* Protected layout with Navbar */}
       <Route
@@ -32,10 +36,10 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
         <Route path="/change-password" element={<ChangePassword />} />
-
-        {/* add more like reports, settings, etc */}
-        <Route path="*" element={<NotFound />} />
       </Route>
+
+      {/* Catch-all route - This MUST be last */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
